658aeb97fe2215afb82a3354995f907caa4bba83
Ozone Bhattarai <ozonebhattarai@gmail.com>
2024-05-25 12:43:47 +0545

    add custom user model, manager, and email the primary username field

--- core_apps/users/models.py
+++ core_apps/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = None
+    USERNAME_FIELD = "email"
+
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    objects = CustomUserManager()
