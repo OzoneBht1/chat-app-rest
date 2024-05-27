5d923c662258af4591c0f0189218d4370b385031
Ozone Bhattarai <ozonebhattarai@gmail.com>
2024-05-27 21:12:35 +0545

    add redis as a channel layer for websockets

--- config/local.py
+++ config/local.py
@@ -1,3 +1,12 @@
 SECRET_KEY = "-47u^b24464!bpl2vse2*l!=+qzgy1cg9qr&z2(*rsa5)-56i!"
 ALLOWED_HOSTS = ["*"]
 CORS_ALLOW_ALL_ORIGINS = True
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("redis://redis:6379/0")],
+        },
+    },
+}
