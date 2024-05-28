3b7043c3acd5b64142c6658f4188bfde6858ddf1
Ozone Bhattarai <ozonebhattarai@gmail.com>
2024-05-28 10:44:18 +0545

    move from query params for token authentication to headers

--- core_apps/chat/middleware.py
+++ core_apps/chat/middleware.py
@@ -19,19 +19,25 @@ def get_user(token_key: str):
 
 
 class TokenAuthMiddleware(BaseMiddleware):
+    """
+    Take the token from headers, and add user or anonymous user to
+    the scope depending upon the validity of the token.
+    """
+
     def __init__(self, inner):
         super().__init__(inner)
 
     async def __call__(self, scope, receive, send):
-        try:
-            token_key = (
-                dict((x.split("=") for x in scope["query_string"].decode().split("&")))
-            ).get("token", None)
-            auth_headers = scope["headers"]
-            print(auth_headers)
-        except ValueError:
-            token_key = None
-        scope["user"] = (
-            AnonymousUser() if token_key is None else await get_user(token_key)
-        )
+        headers = dict(scope["headers"])
+        if b"authorization" in headers:
+            try:
+                _, token_key = headers[b"authorization"].decode().split()
+            except ValueError:
+                token_key = None
+            scope["user"] = (
+                AnonymousUser() if token_key is None else await get_user(token_key)
+            )
+            return await super().__call__(scope, receive, send)
+
+        scope["user"] = AnonymousUser()
         return await super().__call__(scope, receive, send)
